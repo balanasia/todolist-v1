bae024882f20f4e4eefafb93a8392ad53127beb6
Anastasiia Balakhan <anastasiiabalakhan@Anastasiias-MacBook-Air.local>
2022-06-07 15:42:20 -0500

    Add an array that holds new list items. Add for loop in the EJS file to display a new li with a new value o the newListItems array

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const bodyParser = require("body-parser");
 //assigning express to a constant
 const app = express();
 
+//global variable that will hold the user input
+//that is passed to post and get methods
+var items = [];
+
+//specify the format for the BodyParser
+app.use(bodyParser.urlencoded({extended:true}));
+
 //setthing the ejs view engine
 app.set('view engine', 'ejs');
 
@@ -15,49 +22,28 @@ app.get("/", function(req, res) {
 
   //initialize variable that will hold a date variable
   var today = new Date();
-  //get today's day
-  var currentDay = today.getDay();
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var day = days[today.getDay()];
 
-  //emtpy string to hold a day value
-  var day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  switch(currentDay) {
-    case 0:
-      day="Sunday";
-    break;
+  var day = today.toLocaleDateString("en-US", options);
 
-    case 1:
-      day="Monday";
-    break;
-
-    case 2:
-      day="Tuesday";
-    break;
-
-    case 3:
-      day="Wednesday";
-    break;
-
-    case 4:
-      day="Thursday";
-    break;
-
-    case 5:
-      day="Friday";
-    break;
-
-    case 6:
-      day="Saturday";
-    break;
-
-    default:
-      console.log("Error! Day is" + currentDay);
-
-  }//end switch statement
 
   //render the list.ejs file
   //and pass in kindOfDay variable
-  res.render("list", {kindOfDay: day});
+  res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
 });
 
 //express is listening at port 3000
